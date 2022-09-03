3283ca58dcb05bfac8d6e7dddb7aa4f483d4dc26
shipatulhasan <shipatulhasan328@gmail.com>
2022-09-03 08:15:57 +0600

    initialization and header part done

---
+++ script.js
@@ -0,0 +1,112 @@
+// mobile menu
+
+const mobileMenu=()=>{
+
+    const myNav = document.getElementById("myLinks");
+    if (myNav.style.display === "block") {
+        myNav.style.display = "none";
+    } else {
+        myNav.style.display = "block";
+    }
+
+}
+
+
+// load categories and show on the menu
+
+const loadCategories = async()=>{
+
+    const res = await fetch('https://openapi.programming-hero.com/api/news/categories')
+
+    const data = await res.json()
+
+    showCategories (data.data.news_category) 
+
+}
+
+
+const showCategories = (categoryList)=>{
+
+    const menuContainer = document.getElementById('menu-list')
+    menuContainer.innerHTML = `
+    <li class="font-semibold font-poppins text-md text-slate-700 hover:cursor-pointer">Home</li>
+    `
+
+   categoryList.forEach(element => {
+
+    const id = element.category_id
+    const name = element.category_name
+
+    const li = document.createElement('li')
+    li.innerHTML = `
+    <li class="font-semibold font-poppins text-md text-slate-700 hover:cursor-pointer" onclick="loadDetails('${id}','${name}')">${name}</li>
+    `
+    
+    menuContainer.appendChild(li)
+  
+   });
+}
+
+const loadDetails = async(id,name) =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    showDetails(data.data,name)
+}
+
+const showDetails = (data,name)=>{
+    const section = document.getElementById('display-count')
+    section.innerHTML = `
+
+    <h2 class="text-slate-700 font-semibold">
+       ${data.length} items found for category <span class="text-blue">${name}</span>
+      </h2>
+    
+    `
+    const newContainer = document.getElementById('news-container')
+    newContainer.textContent = ''
+
+    data.forEach(element=>{
+        console.log(element)
+
+        const {thumbnail_url,title,details,image_url,total_view,rating,author} =element
+        const {name,img,published_date} = author
+
+        const card = document.createElement('div')
+        card.className = "card lg:card-side bg-base-100 shadow-xl"
+        card.innerHTML =   `
+        
+        <img src="${thumbnail_url}" alt="" class = "p-4">
+        <div class="card-body">
+          <h2 class="card-title">${title}</h2>
+          <p>${details.length > 330 ? `<p>${details.slice(0,234)}<p><br><p> ${details.slice(234,330) + ' . . .'}</p>` : details}</p>
+          
+          <div class="card-actions justify-between items-center grid grid-cols-6 mt-4">
+          <div class="flex gap-5 col-span-3">
+              <div class="overflow-hidden w-16 h-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <img src="${img}" />
+              </div>
+              <div class="description">
+                <h2 class="">
+                ${name === null ||name === '' ?'no data found':name}
+                </h2>
+                <p class="title">
+                 ${published_date === null ||published_date === '' ?'no data found':published_date}
+                </p>
+              </div>
+              </div>
+           
+            <label for="modal-3" class="modal-button py-2 px-4 btn-primary rounded text-white hover:cursor-pointer">view details</label>
+          </div>
+        </div>
+        
+        `
+
+        newContainer.appendChild(card)
+
+    })
+}
+
+loadCategories()
+
+{/* <p>${details.length > 200 ?details.slice(0,100) +". . ." : details }</p> */}
---
+++ test.js
@@ -0,0 +1,181 @@
+<li class="px-4 py-1 font-poppins text-md text-blue bg-purple-600 bg-opacity-10 rounded">Entertainment</li>
+
+
+// spiner function
+
+const spinner = (isTrue)=>{
+
+    const progress = document.getElementById('spinner')
+    if(isTrue){
+        progress.classList.remove('hidden')
+    }
+    else{
+        progress.classList.add('hidden')
+
+    }
+
+}
+
+// laodProduct
+
+const loadProducts = async ()=>{
+
+    const res = await fetch("https://fakestoreapi.com/products")
+    const data = await res.json()
+    return data
+
+}
+
+const showAll = async() =>{
+
+    const data = await (loadProducts())
+    spinner(false)
+
+    const menuContainer = document.getElementById("categories")
+    menuContainer.innerHTML = `
+            <kbd class="kbd hover:cursor-pointer mx-3 bg-gray-100" onclick="displayAll()">all</kbd>`
+    
+    const uniqueArray = []
+    data.forEach(product =>{
+
+        if(uniqueArray.includes(product.category) === false){
+            uniqueArray.push(product.category)
+
+            const tabItem = document.createElement('kbd')
+            
+            tabItem.setAttribute('onclick','searchTab(event)')
+            tabItem.classList.add('kbd','mx-3','hover:cursor-pointer','bg-gray-100')
+            tabItem.innerText = product.category
+            menuContainer.appendChild(tabItem)
+            
+        }
+     
+    })
+    // active color function
+    const tab = menuContainer.querySelectorAll('.kbd')
+    tab.forEach(item=>{
+        item.addEventListener('click',function(e){
+            tab.forEach(element=>element.classList.remove('bg-pink-600','text-white'))
+            e.target.classList.add('bg-pink-600','text-white')
+        })
+    })
+}
+spinner(true)
+showAll()
+
+
+// search condition
+
+const searchData = async(value,isTrue)=>{
+    
+    const data = await loadProducts()
+    spinner(false)
+    let foundProduct
+    if(isTrue){
+        foundProduct =  data.filter(products=>products.category===(value))
+    }
+    else{
+        foundProduct =  data.filter(products=>products.category.includes(value))
+    }
+
+    processing(foundProduct)
+
+
+}
+
+// all tab
+
+const displayAll= ()=>{
+   const all = ''
+    searchData(all,false)
+}
+
+// search by tab
+
+const searchTab = (e)=>{
+    
+    const fieldText = e.target.innerText
+    searchData(fieldText,true)
+
+}
+
+// search field
+
+const searchField = document.getElementById('search-field')
+
+    searchField.addEventListener('keypress',(event)=>{
+    spinner(true)
+
+    const searchValue = searchField.value
+    if(event.key ==='Enter'){
+        document.querySelectorAll('.kbd').forEach(btn=>btn.classList.remove('bg-pink-600','text-white'))
+       searchData(searchValue,false)
+        
+    }
+
+
+})
+
+document.getElementById('del-btn').addEventListener('click',()=>{
+    searchField.value = ''
+    spinner(false)
+})
+
+const processing = (foundProduct)=>{
+        
+        const container = document.getElementById('product-container')
+        const errorMsg = document.getElementById('error-msg')
+        errorMsg.textContent = ''
+        container.textContent = ''
+        if(foundProduct.length == 0){
+            errorMsg.innerHTML = `
+            <h2 class="text-xl text-red-500 fw-bold text-center">
+            Nothing is found
+            </h2>
+            `
+            return
+        }
+
+        foundProduct.forEach(product => {
+
+            const {title, category, price, image, description,rating
+            } = product
+
+            const div = document.createElement('div')
+            div.className = 'card w-full bg-whtie shadow-xl'
+            div.innerHTML =   `
+            
+            <img src="${image}" class="h-52 object-contain p-5" alt="Shoes" />
+            <div class="card-body">
+              <h2 class="card-title">${category}</h2>
+           
+              <p class="truncate" title = "${title}">${title}</p>
+              <div class="card-actions justify-between items-center pt-3">
+                <h2 class="text-pink-600 font-semibold text-xl">$<span class="price">${price}</span> </h2>
+                <label onclick= "display('${title.replace(/[']/g, '')}', '${image}', '${description.replace(/[']/g, '')}','${rating.rate}')" for="modal-3" class="modal-button py-1 px-4 bg-pink-600 rounded text-slate-100 hover:cursor-pointer">view details</label>
+              </div>
+            </div>
+            
+            `
+
+            container.appendChild(div)
+            
+        });
+        
+}
+
+const display = (title,image,description,rating)=>{
+
+    const container = document.getElementById('detail-container')
+    container.innerHTML = `
+        
+            <label for="modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+            <img src="${image}" class="h-52 object-contain p-5" alt="Shoes" />
+            <h3 class="text-lg font-bold">${title}</h3>
+            <p class="text-pink-600 font-bold">Ratings: <span class="text-orange-500">${rating} <sup><i class="fa-solid fa-star text-[10px]"></i></sup><span></p>
+            <p class="py-4">${description}</p>
+            
+        
+      `
+     
+}

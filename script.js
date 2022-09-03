919903b36cd8815d6f6dbad0fded9858bb1b8bd8
shipatulhasan <shipatulhasan328@gmail.com>
2022-09-03 13:56:41 +0600

    desktop view done for news page

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ const showCategories = (categoryList)=>{
 
     const menuContainer = document.getElementById('menu-list')
     menuContainer.innerHTML = `
-    <li class="font-semibold font-poppins text-md text-slate-700 hover:cursor-pointer" onclick = "loadNewses('08','Home')">Home</li>
+    <li class="menu px-4 py-1 font-semibold font-poppins text-md hover:cursor-pointer" onclick = "loadNewses('08','Home')">Home</li>
     `
 
    categoryList.forEach(element => {
@@ -101,7 +101,7 @@ const showNewses = (newses,name)=>{
     
     section.innerHTML = `
 
-    <h2 class="text-slate-700 font-semibold">
+    <h2 class="text-slate-700 font-semibold bg-white rounded p-4 my-5">
        ${newses.length} Newses found for category <span class="text-blue">${name}</span>
       </h2>
     
@@ -121,9 +121,9 @@ const showNewses = (newses,name)=>{
 
     newses.forEach(element=>{
 
-        const {thumbnail_url,title,details,total_view,author} =element
+        let {thumbnail_url,title,details,total_view,author} =element
 
-        const {name,img,published_date} = author
+        let {name,img,published_date} = author
          
         const date = new Date(published_date)
 
@@ -182,11 +182,11 @@ const showNewses = (newses,name)=>{
     })
 }
 
-const loadDetails = async (newsId)=>{
+const loadDetails = async(newsId)=>{
     try{
         const url =  `https://openapi.programming-hero.com/api/news/${newsId}`
-        const res = await fetch(url)
-        const info = await res.json()
+        const response = await fetch(url)
+        const info = await response.json()
         showDetails(info.data[0])
     }
     catch(error){
@@ -195,38 +195,35 @@ const loadDetails = async (newsId)=>{
 }
 
 const showDetails = (information) =>{
-
-    const {title, image_url, total_view,rating, details, author} = information
-    const {name,img,published_date} = author
          
-    const date = new Date(published_date)
+    const mydate = new Date(information.author.published_date)
 
     const container = document.getElementById('details-container')
 
     container.innerHTML = `
     
         <label for="modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-        <div class = "h-5/6" ><img src="${image_url}" class="h-[500px] w-full" alt="${title}" /></div>
+        <div class = "h-5/6" ><img src="${information.image_url}" class="h-[500px] w-full" alt="${information.title}" /></div>
         <div class = "flex justify-between items-center gap-5 py-5">
         <div class = "flex items-center gap-5">
-              <img src="${img}"class="overflow-hidden w-16 h-16 rounded-full ring ring-primary   ring-offset-base-100 ring-offset-2" />
+              <img src="${information.author.img}"class="overflow-hidden w-16 h-16 rounded-full ring ring-primary   ring-offset-base-100 ring-offset-2" />
               <h2 class="font-bold text-dark">
-              ${name ? name :'no data found'}
+              ${information.author.name ? information.author.name :'no data found'}
               </h2>
         </div>
         <div class = "flex gap-5">
         <p class="font-bold">
-        ${published_date ? date.toDateString() : 'no data found'}
+        ${information.author.published_date ? mydate.toDateString() : 'no data found'}
        </p>
        <p class="text-dark text-lg font-bold">
          <i class="fa-sharp fa-solid fa-eye text-blue text-lg"></i> 
-         ${total_view ? total_view : 'not found'}
+         ${information.total_view ? information.total_view : 'not found'}
        </p>
-       <p class="text-blue font-bold">Ratings: <span class="text-orange-500">${rating.number} <sup><i class="fa-solid fa-star text-[10px]"></i></sup><span></p></div>
+       <p class="text-blue font-bold">Ratings: <span class="text-orange-500">${information.rating.number} <sup><i class="fa-solid fa-star text-[10px]"></i></sup><span></p></div>
         
             </div>
-        <h3 class="text-lg font-bold">${title}</h3>
-        <p class="py-4">${details}</p>
+        <h3 class="text-lg font-bold">${information.title}</h3>
+        <p class="py-4">${information.details}</p>
 
     `
     
